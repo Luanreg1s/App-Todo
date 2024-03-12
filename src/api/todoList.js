14912ad89699a8ea2b290d70a9bfeeddde59b719
Luanreg1s <Luanregispereira060@gmail.com>
2024-03-12 20:22:33 -0300

    altera url da aplicaçao

--- src/api/todoList.js
+++ src/api/todoList.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosClient = axios.create({
-    baseURL: "http://localhost:3000"
+    baseURL: "https://api-to-do-list-3.onrender.com"
 });
 
 export async function getTodolists(){
